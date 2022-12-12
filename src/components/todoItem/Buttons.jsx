f79599feabb8e6d322dfb4c15c90804c6585ba05
LeeYujeong <yujlee242@gmail.com>
2022-12-12 20:04:53 +0900

    ✨ 완료 토글, 삭제 기능 추가

--- src/components/todoItem/Buttons.jsx
+++ src/components/todoItem/Buttons.jsx
@@ -1,7 +1,11 @@
 import { Button } from './style';
 
-const Buttons = ({ name, type }) => {
-  return <Button type={type}>{name}</Button>;
+const Buttons = ({ id, name, type, onToggle, onDelete }) => {
+  return (
+    <Button onClick={() => (type === 'done' ? onToggle(id) : onDelete(id))} type={type}>
+      {name}
+    </Button>
+  );
 };
 
 export default Buttons;
--- src/components/todoItem/TodoItem.jsx
+++ src/components/todoItem/TodoItem.jsx
@@ -1,18 +1,35 @@
+import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import Buttons from './Buttons';
 import { TodoItemWrap, Title, Text, More, ButtonWrap } from './style';
+import { toggleDone, deleteTodo } from '../../redux/modules/todos';
 
-const TodoItem = () => {
+const TodoItem = ({ id, title, content, isDone }) => {
+  const dispatch = useDispatch();
+
+  const toggleDoneHandler = (id) => {
+    dispatch(toggleDone({ id }));
+  };
+
+  const deleteTodoHandler = (id) => {
+    if (window.confirm('해당 투두를 정말 삭제하시겠습니까?')) {
+      dispatch(deleteTodo({ id }));
+    }
+  };
+
   return (
     <TodoItemWrap>
-      <Title>📍 제목이 들어가요</Title>
+      <Title>📍 {title}</Title>
       <Text>
-        내용이 들어가요 내용이 들어가요
-        <More>더보기</More>
+        {content}
+        <Link to={`/todos/${id}`}>
+          <More>더보기</More>
+        </Link>
       </Text>
       <ButtonWrap>
-        <Buttons type={'edit'} name={'수정'} />
-        <Buttons type={'delete'} name={'삭제'} />
-        <Buttons type={'done'} name={'완료'} />
+        <Buttons id={id} type={'edit'} name={'수정'} />
+        <Buttons onDelete={deleteTodoHandler} id={id} type={'delete'} name={'삭제'} />
+        <Buttons onToggle={toggleDoneHandler} id={id} type={'done'} name={isDone ? '취소' : '완료'} />
       </ButtonWrap>
     </TodoItemWrap>
   );

20849a3b209cf166b80e67f29760dd9ab863226b
LeeYujeong <yujlee242@gmail.com>
2022-12-12 20:00:53 +0900

    ✨ 라우터 추가 및 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
+import Router from './shared/Router';
 import GlobalStyle from './components/GlobalStyle';
-import Container from './components/Container';
-import Header from './components/header/Header';
-import TodoForm from './components/todoForm/TodoForm';
-import TodoList from './components/todoList/TodoList';
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <Container>
-        <Header />
-        <TodoForm />
-        <TodoList isActive={true} />
-        <TodoList isActive={false} />
-      </Container>
+      <Router />
     </>
   );
 }

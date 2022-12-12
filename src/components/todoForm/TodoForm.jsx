2985ca10179e393b5658e18537c4903591b76e1b
LeeYujeong <yujlee242@gmail.com>
2022-12-12 20:03:22 +0900

    ✨ 투두 추가 기능 추가

--- src/components/todoForm/TodoForm.jsx
+++ src/components/todoForm/TodoForm.jsx
@@ -1,15 +1,63 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { v4 as uuid } from 'uuid';
 import { HiddenTitle } from '../Container';
+import { addTodo } from '../../redux/modules/todos';
 import { FormWrap, Label, Input, Button } from './style';
 
 const TodoForm = () => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const dispatch = useDispatch();
+
+  // 제목 입력값 상태 변화
+  const changeTitleHandler = (event) => {
+    setTitle(event.target.value);
+  };
+
+  // 내용 입력값 상태 변화
+  const changeContentHandler = (event) => {
+    setContent(event.target.value);
+  };
+
+  // 투두 추가
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    if (!title || !content) {
+      alert('제목과 내용을 입력해주세요.');
+      return false;
+    }
+
+    dispatch(
+      addTodo({
+        id: uuid(),
+        title,
+        content,
+        isDone: false,
+      })
+    );
+
+    setTitle('');
+    setContent('');
+    return true;
+  };
+
   return (
     <FormWrap>
       <HiddenTitle>투두 입력</HiddenTitle>
-      <form action="/">
+      <form onSubmit={submitHandler}>
         <Label htmlFor="title">제목</Label>
-        <Input type="text" id="title" />
+        <Input type="text" placeholder="제목" id="title" value={title} onChange={changeTitleHandler} />
         <Label htmlFor="contents">내용</Label>
-        <Input className="contents" type="text" id="contents" />
+        <Input
+          className="contents"
+          type="text"
+          placeholder="무엇을 해야 하나요?"
+          id="contents"
+          value={content}
+          onChange={changeContentHandler}
+        />
         <Button>추가</Button>
       </form>
     </FormWrap>

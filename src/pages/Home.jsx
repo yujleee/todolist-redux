899c863ce167c4d35aca1b5cadf9e9141e19098b
LeeYujeong <yujlee242@gmail.com>
2022-12-12 20:01:35 +0900

    ♻️ Home 페이지 컴포넌트 분리

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import { useSelector } from 'react-redux';
+import Container from '../components/Container';
+import Header from '../components/header/Header';
+import TodoForm from '../components/todoForm/TodoForm';
+import TodoList from '../components/todoList/TodoList';
+
+const Home = () => {
+  const todos = useSelector((state) => state.todos);
+
+  return (
+    <Container>
+      <Header />
+      <TodoForm />
+      <TodoList isActive={true} todos={todos} />
+      <TodoList isActive={false} todos={todos} />
+    </Container>
+  );
+};
+
+export default Home;

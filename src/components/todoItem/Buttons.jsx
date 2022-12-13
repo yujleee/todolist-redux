e7a284e6d79cc400328d006468952233a38ef046
LeeYujeong <yujlee242@gmail.com>
2022-12-13 17:13:49 +0900

    ♻️ onClick 적용 함수 분리

--- src/components/todoItem/Buttons.jsx
+++ src/components/todoItem/Buttons.jsx
@@ -1,8 +1,24 @@
 import { Button } from './style';
 
-const Buttons = ({ id, name, type, onToggle, onDelete }) => {
+const Buttons = ({ id, name, type, onToggle, onDelete, onBack, onEdit }) => {
+  // 클릭 시 타입에 따라 다른 함수가 실행되도록 하는 핸들러
+  const onClickHandler = (type) => {
+    switch (type) {
+      case 'done':
+        return onToggle(id);
+      case 'delete':
+        return onDelete(id);
+      case 'backward':
+        return onBack();
+      case 'edit':
+        return onEdit();
+      default:
+        return null;
+    }
+  };
+
   return (
-    <Button onClick={() => (type === 'done' ? onToggle(id) : onDelete(id))} type={type}>
+    <Button onClick={() => onClickHandler(type)} type={type}>
       {name}
     </Button>
   );

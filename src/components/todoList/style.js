0258ea4a7d7ef14c2d15ff7be0207a5e98a7223a
LeeYujeong <yujlee242@gmail.com>
2022-12-13 17:16:40 +0900

    💄 리스트 컴포넌트 스타일 수정

--- src/components/todoList/style.js
+++ src/components/todoList/style.js
@@ -5,6 +5,10 @@ export const TodoListWrap = styled.section`
   padding: 30px;
   border: 1px solid #eee;
   border-top: none;
+
+  :last-child {
+    margin-bottom: 40px;
+  }
 `;
 
 export const TodoTitle = styled.h1`
@@ -16,4 +20,5 @@ export const List = styled.ul`
   display: flex;
   justify-content: flex-start;
   flex-wrap: wrap;
+  min-height: 200px;
 `;

ad59dfc2c21af9adc44227a8fff5ea9f52c3f515
LeeYujeong <yujlee242@gmail.com>
2023-01-18 17:36:53 +0900

    ♻️ refactoring index.js and Router.js with TS

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -6,7 +5,7 @@ import App from './App';
 import { Provider } from 'react-redux';
 import store from './redux/config/configStore';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <Provider store={store}>
     <App />

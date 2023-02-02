e60f8acc8bac71b4903b7050279790188f55ec51
LeeYujeong <yujlee242@gmail.com>
2023-02-02 15:44:36 +0900

    ✨ setting Recoil

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,11 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
-import { Provider } from 'react-redux';
-import store from './redux/config/configStore';
+import { RecoilRoot } from 'recoil';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <Provider store={store}>
+  <RecoilRoot>
     <App />
-  </Provider>
+  </RecoilRoot>
 );
